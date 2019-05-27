f6a082daddc6e68f65267a10f303fcec21fef200
LyfeTyme <48641165+LyfeTyme@users.noreply.github.com>
2019-05-27 11:28:04 -0700

    closure

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -71,22 +71,37 @@ console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-runners.filter(function(runners){
+let largeShirts = runners.filter(function(runners){
     return runners.shirt_size === "L"
 })
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function(sum, runners){
+    return sum + runners.donation
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given 
+// the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Obtain all emails
+let emailList = [];
+runners.forEach(function(runners){
+    emailList.push(`${runners.first_name} Email is:  ${runners.email}`)
+});
+console.log(emailList);
 
-// Problem 2
-
+// Problem 2 Get Company Name from Skinix
+let companyName = runners.filter(function(runners){
+    return runners.company_name === "Skinix"
+});
+console.log(companyName);
 // Problem 3
+let ticketPriceAvg = runners.reduce(function(sum, runners){
+    return sum + runners.donation / 50;
+}, 0);
+console.log(ticketPriceAvg);
